309bb6ec56f2e93e3c0ef56dfc9694fdaedce227
Belcea Laurentiu <belcealaurentiu@gmail.com>
2024-04-09 17:52:36 +0300

    React Weather App

---
+++ src/api.js
@@ -0,0 +1,12 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1ae2fc52f8msh265ad71427c9440p19577fjsn12caff42e775',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY= "039cb1927531b3e780b8713144a828ea";
